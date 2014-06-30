97858e10894670f6f6f187b2ae872de29f797801
Tyler Childs <tylerwchilds@gmail.com>
2014-06-30 02:28:13 -0400

    pretty print rendering issue 1

--- js/main.js
+++ js/main.js
@@ -10692,23 +10692,22 @@ $(function(){
 			}
 		});
 	}
+});
 
-	// Documentation Show/Hide code functionality
-	// move the code from the code pretty stuff into the example
-  $('.example-wrapper .prettyprint').each(function(){
-    if(!$(this).hasClass("no-duplicate")){
-      var example = $(this).html();
-      $(this).parents('.example-wrapper').find('.example').html(example);
-    }
-  });
+// Documentation Show/Hide code functionality
+// move the code from the code pretty stuff into the example
+$('.example-wrapper .prettyprint').each(function(){
+	if(!$(this).hasClass("no-duplicate")){
+		var example = $(this).html();
+		$(this).parents('.example-wrapper').find('.example').html(example);
+	}
+});
 
-  $(document).on('click', '.code-toggle', function(){
-    $(this)
-      .toggleClass('active')
-      .prev()
-        .slideToggle(200);
-  });
-
+$(document).on('click', '.code-toggle', function(){
+	$(this)
+		.toggleClass('active')
+		.prev()
+		.slideToggle(200);
 });
 
 function b64_to_utf8( str ) {
