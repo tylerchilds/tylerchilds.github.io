9a2a5b0d580353530837e415321163fbe86c2a4f
Tyler Childs <tylerwchilds@gmail.com>
2016-05-26 14:19:24 -0700

    quite a bit of slideshow content

--- js/pages/slideshow.js
+++ js/pages/slideshow.js
@@ -21,14 +21,27 @@ $(document).on("keyup", function(e){
   e.preventDefault();
   e.stopPropagation();
 
-  var key = e.which || e.keyCode || 0;
+  var key = e.which || e.keyCode || -1;
+  navigate(key);
+});
 
+$(document).on("click", function(e){
+  e.preventDefault();
+  e.stopPropagation();
+console.log(e.which);
+  navigate(e.button);
+});
+
+var navigate = function(key){
+  //console.log(key);
   switch(key){
+    case 1: // mouse back
     case 33: // page down
     case 37: // left
     case 38: // up
       Slides.app.vent.trigger("slide:prev");
       break;
+    case 0: // mouse click
     case 13: // enter
     case 34: // page up
     case 39: // right
@@ -38,8 +51,33 @@ $(document).on("keyup", function(e){
     default:
       break;
   }
-});
+};
 
 (function(){
   Slides.app.start();
 })();
+
+
+/*
+
+Examples!
+
+*/
+
+// javascript hover example
+$(document).on('mouseover', '#js-hover', function(){
+  $(this).animate({opacity: 0}, 2000);
+}).on('mouseout', '#js-hover', function(){
+  $(this).animate({opacity: 1}, 2000);
+})
+
+
+// coin toss
+$(document).on('click', '#coin-toss-button', function(e){
+  e.stopPropagation();
+  $('#result').text(tossCoin());
+});
+
+function tossCoin(){
+  return Math.floor(Date.now()) % 2 ? 'heads' : 'tails';
+}
